fddcd1268dec6a5d06f75aa9732bb74418543d3f
Martyna Paliwoda <martynapaliwoda.2001@gmail.com>
2024-11-15 22:30:53 +0100

    Uploading photo mechanism

--- src/Controllers/TimelineController.php
+++ src/Controllers/TimelineController.php
@@ -51,15 +51,57 @@ class TimelineController extends Controller
         header('Location: /');
     }
 
+    private function uploadFile() {
+        $target_dir = "assets/images";
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["image"]["tmp_name"]);
+            if($check !== false) {
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        if ($_FILES["image"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        } else {
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+        return $target_file;
+    }
+
     public function addEvent(int $timelineId) 
     {
         $name = $_POST['name'];
         $shortDescription = $_POST['short_description'];
         $description = $_POST['description'];
         $startDate = $_POST['start_date'];
-        $endDate = $_POST['end_date'];
-        // $imagePath = $_POST['name'];
-        $imagePath = "assets/images/im1.jpg";
+        $endDate = $_POST['end_date'];    
         $result = $this->eventService->addEvent(
             name: $name, 
             shortDescription: $shortDescription, 
@@ -67,7 +109,7 @@ class TimelineController extends Controller
             startDate: $startDate, 
             endDate: $endDate, 
             timelineId: $timelineId, 
-            imagePath: $imagePath
+            imagePath: $this->uploadFile()
         );
         header('Location: /timeline?id=' . $timelineId);
     }
